e1e585a801b03fd2f70ae7ed93ad84c3e3d01d88
Marik <letschuka@gmail.com>
2023-10-16 23:18:39 +0300

    delete unused file

--- Num1.py
+++ Num1.py
@@ -1,95 +1,131 @@
-import string
-
 import numpy as np
 from numpy import ndarray
 import matplotlib.pyplot as plt
 
 
-def null_step_5(matrix: np.ndarray):
+def null_step_5(matrix: np.ndarray) -> np.ndarray:
+    """
+    Set every fifth element of the matrix to zero.
+    """
     matrix.flat[::5] = 0
     return matrix
 
-def null_step_diagonal(matrix: np.ndarray):
+
+def null_step_diagonal(matrix: np.ndarray) -> np.ndarray:
+    """
+    Set the main diagonal of the matrix to zero.
+    """
     np.fill_diagonal(matrix, 0)
     return matrix
 
 
-# Function to visualize a matrix using a heatmap.
-def visualize_matrix(matrix: np.ndarray , title: string):
-    plt.imshow(matrix, cmap='viridis')
-    plt.colorbar()
-    plt.title(title)
-    plt.show()
-
-
-# Function to find the median value of a matrix.
-def find_median(matrix: np.ndarray) -> ndarray:
-    return np.median(matrix)
-
-
-# Function to find the average (mean) value of a matrix.
-def find_average(matrix: ndarray) -> ndarray:
-    return np.mean(matrix)
-
-
-def get_gold_value(mean_value: float, average_value: float):
-    return (mean_value + average_value) / 2
-
-
-def fill_zeros_with_mean(matrix: np.ndarray, gold_value: float) -> np.ndarray:
+def fill_gold(matrix: np.ndarray, gold_value: float) -> np.ndarray:
+    """
+    Replace all zeros in the matrix with a specified gold_value.
+    """
     matrix[matrix == 0] = gold_value
     return matrix
 
 
-# Function to find the nearest value to a given value in the matrix.
+def plot_histogram(matrix, title: str):
+    """
+    Display a histogram of the matrix values.
+    """
+    flattened_data = matrix.flatten()
+    plt.hist(flattened_data, bins=50, color='green', edgecolor='black')
+    plt.title(title)
+    plt.xlabel("Значення")
+    plt.ylabel("Кількість")
+    plt.show()
+
+
+def plot_matrix(matrix: np.ndarray):
+    """
+    Display the matrix as an image.
+    """
+    plt.imshow(matrix, cmap='viridis')
+    plt.colorbar()
+    plt.show()
+
+
+def find_median(matrix: np.ndarray) -> ndarray:
+    """
+    Calculate the median value of the matrix.
+    """
+    return np.median(matrix)
+
+
+def find_average(matrix: ndarray) -> ndarray:
+    """
+    Calculate the average (mean) value of the matrix.
+    """
+    return np.mean(matrix)
+
+
+def get_gold_value(mean_value: float, average_value: float) -> float:
+    """
+    Calculate the average of the mean and average values.
+    """
+    return (mean_value + average_value) / 2
+
+
 def find_nearest_value(matrix: ndarray, value: float) -> float:
-    # Calculate the absolute difference between each matrix element and the given value.
+    """
+    Find the value in the matrix that is closest to a given value.
+    """
     diff = np.abs(matrix - value)
-    # Find the index of the minimum difference.
     index = np.unravel_index(diff.argmin(), diff.shape)
     return matrix[index]
 
 
-# Function to generate a random integer matrix with values between 0 and 100.
 def generate_matrix(rows: int, cols: int) -> ndarray:
-    matrix = np.random.randint(0, 101, (rows, cols))
-    return matrix
+    """
+    Generate a random matrix with integer values between 0 and 100.
+    """
+    return np.random.randint(0, 101, (rows, cols))
 
 
 def main():
-    # Number of rows and columns for the matrix.
-    rows = 13
-    cols = 17
-
-    # Generate a random matrix.
+    """
+    Main function to execute the matrix operations and display results.
+    """
+    # Generate and display a random matrix.
+    rows, cols = 10, 10
     matrix = generate_matrix(rows, cols)
     print(matrix)
-    visualize_matrix(matrix, "original matrix")
+    plot_histogram(matrix, "Original Matrix Histogram")
 
-    # Ask the user to provide a value.
+    # Get user input and display the nearest value in the matrix.
     value = float(input("Please, enter value to check: "))
     print("The nearest value in matrix is:", find_nearest_value(matrix, value))
 
-    # Find and display the average value of the matrix.
+    # Display the average and median values of the matrix.
     average = find_average(matrix)
     print(f"The average value of the matrix is: {average}")
-
-    # Find and display the median value of the matrix.
     median = find_median(matrix)
     print(f"The median value of the matrix is: {median}")
 
+    # Compute the gold_value.
     gold_value = get_gold_value(median, average)
 
+    # Null every fifth element, fill with gold value, and display.
+    matrix_step_5 = null_step_5(matrix.copy())
+    plot_histogram(matrix_step_5, "Matrix with every 5th element nullified")
+    matrix_step_5 = fill_gold(matrix_step_5, gold_value)
+    plot_histogram(matrix_step_5, "Matrix after filling with gold value")
 
-    # Normalize the matrix so that values range between 0 and 1.
-    min_value = matrix.min()
-    max_value = matrix.max()
+    # Nullify the diagonal, fill with gold value, and display.
+    matrix_step_diagonal = null_step_diagonal(matrix.copy())
+    plot_histogram(matrix_step_diagonal, "Matrix with diagonal nullified")
+    matrix_step_diagonal = fill_gold(matrix_step_diagonal, gold_value)
+    plot_histogram(matrix_step_diagonal, "Matrix diagonal filled with gold value")
+
+    # Normalize matrix and display.
+    min_value, max_value = matrix.min(), matrix.max()
     normalized_matrix = (matrix - min_value) / (max_value - min_value)
     print("\nNormalized matrix:")
-    print(normalized_matrix)
-    visualize_matrix(matrix, "normalized")
+    plot_histogram(normalized_matrix, "Normalized Matrix Histogram")
 
 
-# Execute the main function when the script is run.
 if __name__ == '__main__':
     main()
--- Sci1.py
+++ Sci1.py
@@ -1,69 +1,118 @@
+# Import necessary libraries
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter, sobel
+from PIL import Image, ImageEnhance
 
-# Load an image from the given filepath and ensure its size is 600x600 or larger.
-def load_image(filepath):
+
+def load_image(filepath: str) -> np.ndarray:
+    """
+    Load an image from a given filepath and ensure its size is 600x600 or larger.
+    """
     img = plt.imread(filepath)
     if img.shape[0] < 600 or img.shape[1] < 600:
         raise ValueError("Зображення повинно мати розмір 600x600 або більше")
     return img
 
-# Display the image with an optional title and colormap.
-def show_image(image, title="", cmap=None):
+
+def show_image(image: np.ndarray, title: str = "", cmap: str = None):
+    """
+    Display an image with an optional title and colormap.
+    """
     plt.imshow(image, cmap=cmap)
     plt.title(title)
-    plt.axis('off')  # Hide axis labels
+    plt.axis('off')
     plt.show()
 
-# Enhance the contrast of the image.
-def enhance_contrast(img):
-    # Find the minimum and maximum values for each color channel.
-    min_val = 0.02221
-    max_val = 0.1
-    # Normalize and scale the image to enhance contrast.
-    return ((img - min_val) / (max_val - min_val) * 255).astype(np.uint8)
 
-# Convert the color image to grayscale.
-def convert_to_grayscale(img):
+def adjust_contrast(img: Image.Image, level: int) -> Image.Image:
+    """
+    Adjust the contrast of an image based on the provided level.
+    """
+    if not 1 <= level <= 10:
+        raise ValueError("Рівень контрасту повинен бути від 1 до 10")
+
+    enhancer = ImageEnhance.Contrast(img)
+    factor = 1 + (level - 5) * 0.2
+    return enhancer.enhance(factor)
+
+
+def numpy_to_pil(image_np: np.ndarray) -> Image.Image:
+    """
+    Convert a numpy image array to a PIL Image object.
+    """
+    return Image.fromarray((image_np * 255).astype(np.uint8))
+
+
+def convert_to_grayscale(img: np.ndarray) -> np.ndarray:
+    """
+    Convert a color image to grayscale.
+    """
     return np.dot(img[..., :3], [0.299, 0.587, 0.114])
 
-# Split the image into its red, green, and blue channels and display them.
-def split_into_channels(img):
+
+def split_into_channels(img: np.ndarray):
+    """
+    Split the image into its RGB channels and display them.
+    """
     channels = ["Reds", "Greens", "Blues"]
     plt.figure(figsize=(12, 6))
 
     for idx, channel in enumerate(channels):
-        plt.subplot(1, 3, idx+1)
+        plt.subplot(1, 3, idx + 1)
         plt.imshow(img[:, :, idx], cmap=channel, vmin=0, vmax=255)
         plt.title(f"{channel[:-1]} Channel")
         plt.axis('off')
 
     plt.show()
 
-# Apply edge detection to a grayscale image.
-def edge_detection(img_gray):
+
+def edge_detection(img_gray: np.ndarray) -> np.ndarray:
+    """
+    Apply edge detection to a grayscale image.
+    """
     # Compute the Sobel gradient for the x and y directions.
     sx = sobel(img_gray, axis=0, mode='constant')
     sy = sobel(img_gray, axis=1, mode='constant')
+
     # Combine the two gradients to get the edge intensity.
     return np.hypot(sx, sy)
 
-# Apply a Gaussian blur to the image.
-def blur_image(img, sigma=10):
+
+def blur_image(img: np.ndarray, sigma: int = 10) -> np.ndarray:
+    """
+    Apply Gaussian blur to an image.
+    """
     return gaussian_filter(img, sigma=(sigma, sigma, 0))
 
+
 def main():
+    """
+    Main function to load, process and display various versions of an image.
+    """
     image_path = 'audi2.jpg'
     image = load_image(image_path)
 
-    # Display various versions of the image using the defined functions.
+    # Display the original image
     show_image(image, "Оригінальне зображення")
-    show_image(enhance_contrast(image), "З контрастністю")
+
+    # Adjust contrast based on user input and display the image
+    level = int(input("Enter level of contrast from 1 to 10. 5 - middle"))
+    image_pil = numpy_to_pil(image)
+    show_image(adjust_contrast(image_pil, level), "З контрастністю")
+
+    # Display individual RGB channels
     split_into_channels(image)
+
+    # Convert to grayscale and display
     show_image(convert_to_grayscale(image), "Чорно-біле зображення", cmap="gray")
+
+    # Blur and display the image
     show_image(blur_image(image), "Розмите зображення")
+
+    # Apply edge detection and display the result
     show_image(edge_detection(convert_to_grayscale(image)), "Edge Detection", cmap="gray")
+
 
 # Execute the main function when the script is run.
 if __name__ == "__main__":
